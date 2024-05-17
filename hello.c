c1c59ab5502dce90a056ef1d1cf30a6fadb2f99e
Steve Broshar <mister.broshar@yahoo.com>
2024-05-17 22:14:11 +0000

    fix code

--- hello.c
+++ hello.c
@@ -4,13 +4,13 @@
 MODULE_LICENSE("GPL");
 static int init(void)
 {
-    printk(KERN_INFO, "%s: In init\n", __func__);
+    printk(KERN_INFO "%s: In init\n", __func__);
     return 0;
 }
 
 static void exit(void)
 {
-    printk(KERN_INFO, "%s: In exit\n", __func__);
+    printk(KERN_INFO "%s: In exit\n", __func__);
 }
 
 module_init(init);
